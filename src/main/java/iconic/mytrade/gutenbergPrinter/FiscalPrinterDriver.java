873e0cbaefd9a8d065ef734b0fdcb0997acc0178
lino <lino@lino-XPS-13-9370>
2025-10-09 17:14:02 +0200

    TM-5301

--- src/main/java/iconic/mytrade/gutenbergPrinter/FiscalPrinterDriver.java
+++ src/main/java/iconic/mytrade/gutenbergPrinter/FiscalPrinterDriver.java
@@ -1379,7 +1379,7 @@ public class FiscalPrinterDriver implements jpos.FiscalPrinterControl17, StatusU
 	}
 
 	public void statusUpdateOccurred(StatusUpdateEvent arg0) {
-		((StatusUpdateListener) fiscalPrinter).statusUpdateOccurred(arg0);
+		//((StatusUpdateListener) fiscalPrinter).statusUpdateOccurred(arg0);
 		
 	}
 
